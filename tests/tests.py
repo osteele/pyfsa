58a44106d3e3918a17a5a106584d1a91636f9d52
Oliver Steele <steele@osteele.com>
2008-02-04 15:29:37 -0500

    add failing test cases

--- tests/tests.py
+++ tests/tests.py
@@ -7,9 +7,32 @@ class TestFSA(unittest.TestCase):
     def setUp(self):
         pass
     
-    def testshuffle(self):
-        fsa = compileRE('a*')
-        #self.assertEqual(self.seq, range(10))
+    def testsRejects(self):
+        fsa = compileRE('a(b|c*)')
+        self.assertFalse(fsa.accepts("abbb"))
+
+    def testDeterminization(self):
+        states = [0, 1, 2, 3]
+        alphabet = ['a']
+        transitions = [(0, 1, 'a'),
+                       (0, 2, 'a'),
+                       (1, 0, 'b'),
+                       (1, 3, 'a'),
+                       (2, 0, 'a'),
+                       (2, 2, 'b'),
+                       (2, 3, 'a')]
+        initialState = 0
+        finalStates = [3]
+        fsa = FSA(states, alphabet, transitions, initialState, finalStates)
+        self.assertTrue(fsa.accepts('aba'))
+        dfa = fsa.determinized()
+        self.assertTrue(dfa.accepts('aba'))
+
+    def testMinimization(self):
+        fsa = concatenation(singleton("a"),minimize(closure(singleton("b"))))
+        self.assertFalse(fsa.accepts("b"))
+        fsa = concatenation(singleton("a"),closure(singleton("b")))
+        self.assertFalse(fsa.accepts("b"))
 
 if __name__ == '__main__':
     unittest.main()
